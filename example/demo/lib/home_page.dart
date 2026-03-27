1434f767ee05cc826d4a92b137a26f1194572c24
Sandip <sandipramanik07@gmail.com>
2026-03-27 10:21:30 +0530

    fix: dart format

--- example/demo/lib/home_page.dart
+++ example/demo/lib/home_page.dart
@@ -285,7 +285,7 @@ class _PeerScreenState extends State<PeerScreen> {
                 : ListView.separated(
                     padding: const EdgeInsets.all(12),
                     itemCount: todos.length,
-                    separatorBuilder: (_, __) => const SizedBox(height: 8),
+                    separatorBuilder: (_, _) => const SizedBox(height: 8),
                     itemBuilder: (context, index) {
                       return TodoItemWidget(
                         text: todos[index].toString(),
